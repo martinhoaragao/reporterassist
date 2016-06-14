65c9f5f5a9e6b077f9dad77667de983f3aaef3e1
BrunoJAzevedo <bruno.azevedo.brg94@gmail.com>
2016-06-14 07:25:17 +0100

    Started desktop version

---
+++ src/ReporterAssist/ReporterAssist/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace ReporterAssist
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
